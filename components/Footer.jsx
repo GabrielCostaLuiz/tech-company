cd4102a4e8e6f138368a1b1bc34ad8050b438531
Gabriel Costa Luiz <53578058+GabrielCostaLuiz@users.noreply.github.com>
2023-10-01 19:03:12 -0300

    Footer arrumado

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='border-b-[5rem] border-black'>
+    <footer className=' pb-5'>
       <img src="/assets/footer-action.svg" alt="" />
+
+      <p className='px-14 pt-5'>Website programmed by Gabriel Costa Luiz: <a href="https://gabrielcostaluiz-portfolio.vercel.app/" className='text-red-500'>Portfolio</a> </p>
+          <p className='px-14 pt-5'>Figma by Fueler: <a href="https://www.figma.com/community/file/1132406772863980181" target='_blank' className='text-red-500'>Clique aqui para ir para o figma do projeto.</a></p>
     </footer>
   )
 }
