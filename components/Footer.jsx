9e323da87dd7763df72a412dbdfbc211dd95b99f
Gabriel Costa Luiz <53578058+GabrielCostaLuiz@users.noreply.github.com>
2023-12-26 09:02:50 -0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className=' pb-5'>
-      <img src="/assets/footer-action.svg" alt="" />
+      <img src="/assets/footer-action.svg" alt="" className='min-w-full'/>
 
       <p className='px-14 pt-5'>Website programmed by Gabriel Costa Luiz: <a href="https://gabrielcostaluiz-portfolio.vercel.app/" className='text-red-500'>Portfolio</a> </p>
           <p className='px-14 pt-5'>Figma by Fueler: <a href="https://www.figma.com/community/file/1132406772863980181" target='_blank' className='text-red-500'>Clique aqui para ir para o figma do projeto.</a></p>
